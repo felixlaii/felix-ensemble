035174653166c1bb100be46c8ec399626b0cb147
felixlaii <felix.lai@hotmail.com>
2022-06-12 18:29:00 -0400

    added usestate

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
+import { omdbEndPoint, APIKEY } from "../../utils/omdbApi";
+import axios from "axios";
 
 function SearchBar() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+
   return (
     <div>SearchBar</div>
   )
