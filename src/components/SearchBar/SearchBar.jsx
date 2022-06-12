9064a227f98c1763efe6089b8250db2aa24886bf
felixlaii <felix.lai@hotmail.com>
2022-06-12 18:29:32 -0400

    created fetchdata function to get api response

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,6 +6,20 @@ function SearchBar() {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(false);
   
+    const fetchData = (search) => {
+        if (search.length > 1) {
+          setLoading(true);
+          axios
+            .get(`${omdbEndPoint}?s=${search}&${APIKEY}`)
+            .then((response) => {
+              if (response.data.Search) {
+                setMovies(response.data.Search);
+                setLoading(false);
+              }
+            })
+            .catch((error) => console.log(error));
+        }
+      };
 
   return (
     <div>SearchBar</div>
