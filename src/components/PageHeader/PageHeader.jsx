d2788ca20e76413b5a2e0f307db5524a0dca9712
felixlaii <felix.lai@hotmail.com>
2022-06-12 18:01:37 -0400

    added app title

---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pageHeader.scss";
+
+function PageHeader() {
+  return (
+    <header className="page-header">
+      <h1 className="page-header__title">OMDB</h1>
+    </header>
+  );
+}
+
+export default PageHeader;
