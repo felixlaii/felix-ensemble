541c21172ef2cf1016a3719d77d81036133d0f0f
felixlaii <felix.lai@hotmail.com>
2022-06-12 18:42:06 -0400

    added search bar into app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import PageHeader from "./components/PageHeader/PageHeader";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <PageHeader />
+      <SearchBar />
     </div>
   );
 }
